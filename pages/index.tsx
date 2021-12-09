c9bfb9b55d20e60f588be38f557497710ac08fdd
martindevaluado <pmartinacosta@gmail.com>
2021-12-09 00:42:16 -0300

    conffeti

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,11 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import confetti from 'https://cdn.skypack.dev/canvas-confetti'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={() => confetti()}>
       <Head>
         <title>Paisa Cumple</title>
         <meta name="description" content="Paisa Cumple" />
@@ -14,7 +15,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hoy <strong>SI</strong> es el cumple de <a href="https://paisanos.io">Sebi!</a>
+          Hoy <strong>SI</strong> es el cumple de <a href="#">Sebi!</a>
           <Image src="/sebi.jpg" alt="Sebi NO cumple" width={250} height={350} />
         </h1>
       </main>
